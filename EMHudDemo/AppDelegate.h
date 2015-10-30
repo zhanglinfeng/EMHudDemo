7e1b894aee92d9904a8b91d3793f997376b69445
张林峰 <zhanglinfeng@zhanglinfengdeMacBook-Pro.local>
2015-10-30 09:31:16 +0800

    Initial Commit

---
+++ EMHudDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EMHudDemo
+//
+//  Created by 张林峰 on 15/10/30.
+//  Copyright (c) 2015年 张林峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EMHudDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EMHudDemo
+//
+//  Created by 张林峰 on 15/10/30.
+//  Copyright (c) 2015年 张林峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
